8f8215db853c95d0c675a82e665c902bad18370f
Space Drawing <kulevojdanil@gmail.com>
2025-03-27 23:44:09 +0300

    feat: add greeting and two_params routes with HTML responses; create .gitignore

--- main.py
+++ main.py
@@ -78,6 +78,47 @@ def promotion_image():
 </html>"""
 
 
+@app.route("/greeting/<username>")
+def greeting(username):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Привет, {username}</title>
+                  </head>
+                  <body>
+                    <h1>Привет, {username}!</h1>
+                  </body>
+                </html>"""
+
+
+@app.route("/two_params/<username>/<int:number>")
+def two_params(username, number):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Пример с несколькими параметрами</title>
+                  </head>
+                  <body>
+                    <h2>{username}</h2>
+                    <div>Это первый параметр и его тип: {str(type(username))[1:-1]}</div>
+                    <h2>{number}</h2>
+                    <div>Это второй параметр и его тип: {str(type(number))[1:-1]}</div>
+                  </body>
+                </html>"""
+
+
 @app.route("/astronaut_selection")
 def astronaut_selection():
     return render_template("astronaut_selection.html")
